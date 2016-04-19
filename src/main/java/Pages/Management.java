6a7e4013f289d82c3b2110af7cd94020f23796d5
sanyatolok <sanyatolok@gmail.com>
2016-04-19 13:58:59 +0300

    Add new test cases

--- src/main/java/Pages/Management.java
+++ src/main/java/Pages/Management.java
@@ -4,6 +4,14 @@ import Elements.Button;
 import Enums.Variables;
 import MainSettings.Settings;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+
+import java.util.Iterator;
+import java.util.Set;
 
 public class Management extends Settings
 {
@@ -25,6 +33,42 @@ public class Management extends Settings
         company_link.click();
         open_menu.click();
         management_link.click();
+        WebElement ceo_facebook = driver.findElement(By.xpath(Variables.CEO_FACEBOOK.toString()));
+        Actions newwin = new Actions(driver);
+        newwin.keyDown(Keys.SHIFT).click(ceo_facebook).keyUp(Keys.SHIFT).build().perform();
+        Settings.waitInSeconds(5);
+        String parentWindowHandler = driver.getWindowHandle(); // Store your parent window
+        String subWindowHandler = null;
+        String subWindowHandler2 = null;
+        Set<String> handles = driver.getWindowHandles(); // get all window handles
+        Iterator<String> iterator = handles.iterator();
+        while(iterator.hasNext())
+        {
+            subWindowHandler = iterator.next();
+
+        }
+        driver.switchTo().window(subWindowHandler); // switch to popup window
+        String url_facebook = driver.getCurrentUrl();
+        if(url_facebook.contains("https://www.facebook.com/andrey.davidov.524?fref=ts")) {
+            driver.close();
+        }
+        else
+        {
+            Assert.fail("Test failed");//show error message if link is incorrect
+
+        }
+        driver.switchTo().window(parentWindowHandler);
+        ceo_linked_in.click();
+        String url_linkedin = driver.getCurrentUrl();
+        if(url_linkedin.contains("https://www.linkedin.com/in/andrey-davidov-3518921a"))
+        {
+            System.out.print("link equal to https://www.linkedin.com/in/andrey-davidov-3518921a");
+        }
+        else
+        {
+            Assert.fail("Test failed 2");//show error message if link is incorrect
+
+        }
         return new Management();
     }
     public static Management verify_links_bdm_Europa()

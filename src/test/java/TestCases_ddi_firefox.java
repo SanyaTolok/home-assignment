dd87e7da6456c7a2c5fe7edd4fc17f4f76e83250
sanyatolok <sanyatolok@gmail.com>
2016-04-19 16:23:04 +0300

    Add new test cases

--- src/test/java/TestCases_ddi_firefox.java
+++ src/test/java/TestCases_ddi_firefox.java
@@ -14,98 +14,49 @@ public class TestCases_ddi_firefox extends Settings
     @Test(priority = 0)
     public void verify_links_pr_manager(){Management.verify_links_pr_manager();}
     @Test(priority = 0)
-    public void paging_portfolio()
-    {
-        Portfolio.paging_portfolio();
-    }
+    public void paging_portfolio() {Portfolio.paging_portfolio();}
     @Test(priority = 0)
-    public void zoom_map_contact_us()
-    {
-        Contact_US.zoom_in_and_zoom_out_map();
-    }
+    public void zoom_map_contact_us() {Contact_US.zoom_in_and_zoom_out_map();    }
     @Test(priority = 0,invocationCount=1)
     public void add_discuss_comment() {Blog.discuss_add_comment();}
     @Test(priority = 0)
-    public void company_links_verify_present()
-{
-    Company.company_links_present();
-}
+    public void company_links_verify_present(){Company.company_links_present();}
     @Test(priority = 0)
-    public void test_paging_blog()
-    {
-        Blog.test_paging();
-    }
+    public void test_paging_blog() {Blog.test_paging(); }
     @Test(priority = 0)
-    public void portfolio_tabs_exsist_and_works()
-    {
-        Portfolio.tabs_exsist_and_works();
-    }
+    public void portfolio_tabs_exsist_and_works() { Portfolio.tabs_exsist_and_works();    }
     @Test(priority = 0)
-    public void get_more_info_portfolio()    {
-        Portfolio.get_more_info();}
+    public void get_more_info_portfolio(){Portfolio.get_more_info();}
     @Test(priority = 0)
-    public void send_request()
-    {
-        HomePage.send_request();
-    }
+    public void send_request() {   HomePage.send_request();    }
     @Test(priority = 0)
     public void verify_subscription() {HomePage.subscribe_email();}
     @Test(priority = 0)
-    public void test_blog_tabs()
-    {
-        Blog.all_tabs_exsist();
-    }
+    public void test_blog_tabs() {  Blog.all_tabs_exsist();}
     @Test(priority = 0)
-    public void share_blog_facebook()
-    {
-        Blog.share_facebook();
-    }
+    public void share_blog_facebook() {Blog.share_facebook();}
     @Test(priority = 0)
-    public void share_blog_tweet()
-    {
-        Blog.share_tweet();
-    }
+    public void share_blog_tweet() { Blog.share_tweet();}
     @Test(priority = 0)
     public void share_blog_linked_in()  {Blog.share_linked_in();}
     @Test(priority = 0)
     public void send_request_contact_us()  {Contact_US.send_request();}
     @Test(priority = 0)
-    public void test_services_form()
-    {
-        Services.get_request_from_services_page();
-    }
+    public void test_services_form() {Services.get_request_from_services_page();}
     @Test(priority = 1)
-    public void services_link_present()
-    {
-        HomePage.services_link_present();
-    }
+    public void services_link_present() {HomePage.services_link_present();}
     @Test(priority = 1)
-    public void blog_link_present()
-    {
-        HomePage.blog_link_present();
-    }
+    public void blog_link_present() {HomePage.blog_link_present();}
     @Test(priority = 1)
     public void company_link_present()   { HomePage.company_link_present(); }
     @Test(priority = 1)
-    public void contuct_us_link_present()
-    {
-        HomePage.contuct_us_link_present();
-    }
+    public void contuct_us_link_present() {HomePage.contuct_us_link_present();    }
     @Test(priority = 1)
-    public void portfolio_link_present()
-    {
-        HomePage.portfolio_link_present();
-    }
+    public void portfolio_link_present() {HomePage.portfolio_link_present();}
     @Test(priority = 1)
-    public void technology_link_present()
-    {
-        HomePage.technology_link_present();
-    }
+    public void technology_link_present() {HomePage.technology_link_present();}
     @Test(priority = 1)
-    public void get_subscription_cookie()
-    {
-        Technology.get_coockie();
-    }
+    public void get_subscription_cookie() {Technology.get_coockie();}
     //    @Test(priority = 0)
 //    public void remove_comment_blog()
 //    {

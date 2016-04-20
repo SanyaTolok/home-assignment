442e78a1983535bee14bbfc70f66572c894e0a93
sanyatolok <sanyatolok@gmail.com>
2016-04-20 17:13:22 +0300

    change browser

--- src/main/java/MainSettings/Settings.java
+++ src/main/java/MainSettings/Settings.java
@@ -21,7 +21,7 @@ public class Settings {
     @BeforeMethod
     public void setUp()
     {
-        driver = new EventFiringWebDriver(new PhantomJSDriver()).register(eventListener);
+        driver = new EventFiringWebDriver(new FirefoxDriver()).register(eventListener);
         getDriver().get(baseURL);
         mainPage = new HomePage();
         driver.manage().window().maximize();

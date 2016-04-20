8813b872debf44d74bb264bee3f64ca41e803545
sanyatolok <sanyatolok@gmail.com>
2016-04-20 15:29:01 +0300

    Add new test cases

--- src/main/java/Pages/Management.java
+++ src/main/java/Pages/Management.java
@@ -15,15 +15,11 @@ import java.util.Set;
 
 public class Management extends Settings
 {
-    private static Button ceo_facebook=new Button(By.xpath(Variables.CEO_FACEBOOK.toString()));
+
     private static Button ceo_linked_in=new Button(By.xpath(Variables.CEO_LINKED_IN.toString()));
-    private static Button bdm_europa_facebook=new Button(By.xpath(Variables.BDM_EUROPA_FACEBOOK.toString()));
     private static Button bdm_europa_linked_in=new Button(By.xpath(Variables.BDM_EUROPA_LINKED_IN.toString()));
-    private static Button bdm_america_facebook=new Button(By.xpath(Variables.BDM_AMERICA_FACEBOOK.toString()));
     private static Button bdm_america_linked_in=new Button(By.xpath(Variables. BDM_AMERICA_LINKED_IN.toString()));
-    private static Button hdm_facebook=new Button(By.xpath(Variables.HMD_FACEBOOK.toString()));
     private static Button hmd_linked_in=new Button(By.xpath(Variables.HMD_LINKED_IN.toString()));
-    private static Button pr_facebook=new Button(By.xpath(Variables.PR_FACEBOOK.toString()));
     private static Button pr_linked_in=new Button(By.xpath(Variables.PR_LINKED_IN.toString()));
     private static Button company_link=new Button(By.xpath(Variables.COMPANY_LINK.toString()));
     private static Button open_menu=new Button(By.xpath(Variables.OPEN_MENU.toString()));
@@ -39,7 +35,6 @@ public class Management extends Settings
         Settings.waitInSeconds(5);
         String parentWindowHandler = driver.getWindowHandle(); // Store your parent window
         String subWindowHandler = null;
-        String subWindowHandler2 = null;
         Set<String> handles = driver.getWindowHandles(); // get all window handles
         Iterator<String> iterator = handles.iterator();
         while(iterator.hasNext())
@@ -66,7 +61,7 @@ public class Management extends Settings
         }
         else
         {
-            Assert.fail("Test failed 2");//show error message if link is incorrect
+            Assert.fail("Test failed, linkedin link incorrect");//show error message if link is incorrect
 
         }
         return new Management();
@@ -76,6 +71,41 @@ public class Management extends Settings
         company_link.click();
         open_menu.click();
         management_link.click();
+        WebElement bdm_europa_facebook = driver.findElement(By.xpath(Variables.BDM_EUROPA_FACEBOOK.toString()));
+        Actions newwin = new Actions(driver);
+        newwin.keyDown(Keys.SHIFT).click(bdm_europa_facebook).keyUp(Keys.SHIFT).build().perform();
+        Settings.waitInSeconds(5);
+        String parentWindowHandler = driver.getWindowHandle(); // Store your parent window
+        String subWindowHandler = null;
+        Set<String> handles = driver.getWindowHandles(); // get all window handles
+        Iterator<String> iterator = handles.iterator();
+        while(iterator.hasNext())
+        {
+            subWindowHandler = iterator.next();
+
+        }
+        driver.switchTo().window(subWindowHandler); // switch to popup window
+        String url_facebook = driver.getCurrentUrl();
+        if(url_facebook.contains("https://www.facebook.com/roma.dereka")) {
+            driver.close();
+        }
+        else
+        {
+            Assert.fail("Test failed");//show error message if link is incorrect
+
+        }
+        driver.switchTo().window(parentWindowHandler);
+        bdm_europa_linked_in.click();
+        String url_linkedin = driver.getCurrentUrl();
+        if(url_linkedin.contains("https://ua.linkedin.com/in/roman-dereka-89a80259"))
+        {
+            System.out.print("link equal to https://ua.linkedin.com/in/roman-dereka-89a80259");
+        }
+        else
+        {
+            Assert.fail("Test failed, linkedin link incorrect");//show error message if link is incorrect
+
+        }
         return new Management();
     }
     public static Management verify_links_bdm_America()
@@ -83,6 +113,41 @@ public class Management extends Settings
         company_link.click();
         open_menu.click();
         management_link.click();
+        WebElement bdm_america_facebook = driver.findElement(By.xpath(Variables.BDM_AMERICA_FACEBOOK.toString()));
+        Actions newwin = new Actions(driver);
+        newwin.keyDown(Keys.SHIFT).click(bdm_america_facebook).keyUp(Keys.SHIFT).build().perform();
+        Settings.waitInSeconds(5);
+        String parentWindowHandler = driver.getWindowHandle(); // Store your parent window
+        String subWindowHandler = null;
+        Set<String> handles = driver.getWindowHandles(); // get all window handles
+        Iterator<String> iterator = handles.iterator();
+        while(iterator.hasNext())
+        {
+            subWindowHandler = iterator.next();
+
+        }
+        driver.switchTo().window(subWindowHandler); // switch to popup window
+        String url_facebook = driver.getCurrentUrl();
+        if(url_facebook.contains("https://www.facebook.com/shyhimaha")) {
+            driver.close();
+        }
+        else
+        {
+            Assert.fail("Test failed");//show error message if link is incorrect
+
+        }
+        driver.switchTo().window(parentWindowHandler);
+        bdm_america_linked_in.click();
+        String url_linkedin = driver.getCurrentUrl();
+        if(url_linkedin.contains("https://ua.linkedin.com/in/antonshygimaga"))
+        {
+            System.out.print("link equal to https://ua.linkedin.com/in/antonshygimaga");
+        }
+        else
+        {
+            Assert.fail("Test failed, linkedin link incorrect");//show error message if link is incorrect
+
+        }
         return new Management();
     }
     public static Management verify_links_head_marketing_department()
@@ -90,6 +155,41 @@ public class Management extends Settings
         company_link.click();
         open_menu.click();
         management_link.click();
+        WebElement hmd_facebook = driver.findElement(By.xpath(Variables.HMD_FACEBOOK.toString()));
+        Actions newwin = new Actions(driver);
+        newwin.keyDown(Keys.SHIFT).click(hmd_facebook).keyUp(Keys.SHIFT).build().perform();
+        Settings.waitInSeconds(5);
+        String parentWindowHandler = driver.getWindowHandle(); // Store your parent window
+        String subWindowHandler = null;
+        Set<String> handles = driver.getWindowHandles(); // get all window handles
+        Iterator<String> iterator = handles.iterator();
+        while(iterator.hasNext())
+        {
+            subWindowHandler = iterator.next();
+
+        }
+        driver.switchTo().window(subWindowHandler); // switch to popup window
+        String url_facebook = driver.getCurrentUrl();
+        if(url_facebook.contains("https://www.facebook.com/alex.kuzenko1")) {
+            driver.close();
+        }
+        else
+        {
+            Assert.fail("Test failed");//show error message if link is incorrect
+
+        }
+        driver.switchTo().window(parentWindowHandler);
+        hmd_linked_in.click();
+        String url_linkedin = driver.getCurrentUrl();
+        if(url_linkedin.contains("https://ua.linkedin.com/in/alexey-kutsenko-12807566"))
+        {
+            System.out.print("link equal to https://ua.linkedin.com/in/alexey-kutsenko-12807566");
+        }
+        else
+        {
+            Assert.fail("Test failed, linkedin link incorrect");//show error message if link is incorrect
+
+        }
         return new Management();
     }
     public static Management verify_links_pr_manager()
@@ -97,6 +197,41 @@ public class Management extends Settings
         company_link.click();
         open_menu.click();
         management_link.click();
+        WebElement pr_facebook = driver.findElement(By.xpath(Variables.PR_FACEBOOK.toString()));
+        Actions newwin = new Actions(driver);
+        newwin.keyDown(Keys.SHIFT).click(pr_facebook).keyUp(Keys.SHIFT).build().perform();
+        Settings.waitInSeconds(5);
+        String parentWindowHandler = driver.getWindowHandle(); // Store your parent window
+        String subWindowHandler = null;
+        Set<String> handles = driver.getWindowHandles(); // get all window handles
+        Iterator<String> iterator = handles.iterator();
+        while(iterator.hasNext())
+        {
+            subWindowHandler = iterator.next();
+
+        }
+        driver.switchTo().window(subWindowHandler); // switch to popup window
+        String url_facebook = driver.getCurrentUrl();
+        if(url_facebook.contains("https://www.facebook.com/marisha.blinova")) {
+            driver.close();
+        }
+        else
+        {
+            Assert.fail("Test failed");//show error message if link is incorrect
+
+        }
+        driver.switchTo().window(parentWindowHandler);
+        pr_linked_in.click();
+        String url_linkedin = driver.getCurrentUrl();
+        if(url_linkedin.contains("https://ua.linkedin.com/in/marina-blinova-1b9385103"))
+        {
+            System.out.print("link equal to https://ua.linkedin.com/in/marina-blinova-1b9385103");
+        }
+        else
+        {
+            Assert.fail("Test failed, linkedin link incorrect");//show error message if link is incorrect
+
+        }
         return new Management();
     }
 
